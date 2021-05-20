92e0fbf9c581a2c21c85ba9871c66ec18637df7f
amberm1647 <35271595+amberm1647@users.noreply.github.com>
2021-05-19 17:38:51 -0700

    quick fixes

--- Final Panel/panel_init.py
+++ Final Panel/panel_init.py
@@ -9,21 +9,17 @@ client = HALE.Interface()
 
 main_panel = client.add_panel(HALE.Panel(size_x = 1600, size_y = 800))
 engine_panel = client.add_panel(HALE.Panel(size_x = 1600, size_y = 800))
-
-sensor_panel = client.add_panel(HALE.Panel(size_x = 1600, size_y = 800))
-
+indicator_panel = client.add_panel(HALE.Panel(size_x = 1600, size_y = 800, bgcolor=None, background_image=tk.PhotoImage(file="./IndicatorPanelBg")))
 
 
 #Logos
 background_image = tk.PhotoImage(file = "./hale.png", width=100, height=60)
 main_panel.add_element(HALE.Image(pos_x = 0, pos_y = 0, width=100, height=60, image=background_image))
 engine_panel.add_element(HALE.Image(pos_x = 0, pos_y = 0, width=100, height=60, image=background_image))
-indicator_panel = client.add_panel(HALE.Panel(size_x = 1600, size_y = 800, bgcolor=None, background_image=tk.PhotoImage(file="./IndicatorPanelBg")))
+indicator_panel.add_element(HALE.Image(pos_x = 0, pos_y = 0, width=100, height=60, image=background_image))
 
-background_label3 = tk.Label(sensor_panel, image=background_image)
+background_label3 = tk.Label(indicator_panel, image=background_image)
 background_label3.place(x=0, y=0, width=100, height=60)
-
-indicator_panel = client.add_panel(HALE.Panel(size_x = 1600, size_y = 800, background_image=tk.PhotoImage(file="./IndicatorPanelBg")))
 
 
 
