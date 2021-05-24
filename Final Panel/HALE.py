59873ac768c7b40a77805cc138a677fc97e59e16
bagchia <bagchia@oregonstate.edu>
2021-05-24 00:19:34 -0700

    cleaned things up

--- Final Panel/HALE.py
+++ Final Panel/HALE.py
@@ -8,7 +8,7 @@ from indicatorlight import *
 from rectangle import *
 from image import *
 from loggingbox import *
-from new_chart import *
+from chart import *
 from valveindicator import *
 from numericdisplay import *
 from barchart import *
