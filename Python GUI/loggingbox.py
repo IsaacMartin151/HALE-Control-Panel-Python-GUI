d797886064d7fa3568fed8d397ffb64d83cbbc64
amberm1647 <35271595+amberm1647@users.noreply.github.com>
2021-05-24 19:55:06 -0700

    unfixed logging box-- back to add_message

--- Python GUI/loggingbox.py
+++ Python GUI/loggingbox.py
@@ -4,19 +4,18 @@ import element
 import time
 
 class LoggingBox(element.Element):
-    def __init__(self, *, font=("Arial", 12), text_color="black", bgcolor="white", get_data=None, **kwargs):
+    def __init__(self, *, font=("Arial", 12), text_color="black", bgcolor="white", **kwargs):
         self.font = font
         self.text_color = text_color
         self.bgcolor = bgcolor
-        self.get_data = get_data
         super().__init__(**kwargs)
 
     def display_content(self):
         self.lb = tk.Listbox(self.containing_frame, width=self.abs_size_x, font=self.font, fg=self.text_color)
         self.lb.pack(expand=True, fill="both")
         
+    def add_message(self, text="Message"):
+        self.lb.insert(tk.END, text)
 
     def update(self): 
-        if (self.get_data):
-            self.lb.insert(tk.END, self.get_data)
-        
+        pass
